734a674bc45c9712a5846cee17debeb318b0c6d3
Dj Isaac <contact@dejaydev.com>
2025-03-26 17:45:11 -0500

    :bug: turbo

--- builder/fetch_models.py
+++ builder/fetch_models.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from faster_whisper import WhisperModel
 
-model_names = ["medium", "large-v3-turbo"]
+model_names = ["medium", "turbo"]
 
 
 def load_model(selected_model):
