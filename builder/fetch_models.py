3f76b0913d6f5d5f2aab6b55f17a80d8ce9b88fd
Dj Isaac <contact@dejaydev.com>
2025-03-26 17:33:25 -0500

    :arrow_up: Upgrade from large-v3 to turbo

--- builder/fetch_models.py
+++ builder/fetch_models.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from faster_whisper import WhisperModel
 
-model_names = ["medium", "large-v3"]
+model_names = ["medium", "large-v3-turbo"]
 
 
 def load_model(selected_model):
